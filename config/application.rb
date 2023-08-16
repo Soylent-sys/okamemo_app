1f7a6d547fe27e427eb8782d441a969e32aea336
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2023-08-17 02:03:09 +0900

    [add]アプリ日本語化のためrails-i18nのgithubのja.ymlロケールファイルの設置と設定を実施する

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,9 @@ module OkamemoApp
     config.time_zone = "Asia/Tokyo"
     config.active_record.default_timezone = :local
 
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: false,
