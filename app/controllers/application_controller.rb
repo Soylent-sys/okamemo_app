83b76338a9d86297e39bb17f8198269887b3dac1
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2023-08-21 19:17:07 +0900

    [add]TOPページを作成するためhomeコントローラーの生成とルートの設定を実施する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,2 @@
 class ApplicationController < ActionController::Base
-  # テスト表示用のpage
-  def test
-    render body: "Test TOP Page"
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
+  root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-
-  # テスト用のルーティングを追加
-  get "/", to: "application#test"
 end
