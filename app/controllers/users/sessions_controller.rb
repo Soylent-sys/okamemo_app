28e57c0acf22e11932b0bb9d442443d770f6f678
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2023-09-05 22:32:32 +0900

    [add]deviseでカスタムコントローラーが使用できるように設定を実施する

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  before_action :configure_sign_in_params, only: [:create]
+
+  # GET /users/sign_in
+  def new
+    super
+  end
+
+  # POST /users/sign_in
+  def create
+    super
+  end
+
+  # DELETE /users/sign_out
+  def destroy
+    super
+  end
+
+  protected
+
+  # 許可するための追加のパラメータがある場合は、sanitizer に追加してください
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  end
+end
