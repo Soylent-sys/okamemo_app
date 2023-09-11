0af9b0c4a1c23100fef84bb60360b1cd6d7da57e
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2023-09-11 19:19:48 +0900

    [add]usersテーブルの:nameカラムにNotnull制約を追加する

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_01_113417) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_11_093416) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
+    t.string "name", default: "", null: false
     t.boolean "admin", default: false, null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
