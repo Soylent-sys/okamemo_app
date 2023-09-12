d57d5c03ba4767b0e7f67578fa8d5e4076288940
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2023-09-12 18:10:56 +0900

    [add]セキュリティ強化のためUserリソースのバリデーションを追加する

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-  validates :name, presence: true, length: { maximum: 20 }
+  MAX_LENGTH_EMAIL = 255
+  MAX_LENGTH_NAME = 20
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+
+  validates :email, length: { maximum: MAX_LENGTH_EMAIL }
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  validates :name, presence: true, length: { maximum: MAX_LENGTH_NAME }
 end
