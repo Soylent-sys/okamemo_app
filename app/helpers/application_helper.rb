2b9b7d667bbb40281f347c0e7f329af257d4a54e
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2023-09-18 20:27:00 +0900

    [add]ログイン・非ログイン時のTOPページビューを作成する

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
   def full_title(title: '')
     title.blank? ? "#{BASE_TITLE} - 買い物お助けサービス" : "#{title} - #{BASE_TITLE}"
   end
+
+  def html_safe_newline(str)
+    h(str).gsub(/\n|\r|\r\n/, "<br>").html_safe
+  end
 end
