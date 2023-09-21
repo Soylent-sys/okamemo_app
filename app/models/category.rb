8498a78018a7fba084bff4982aa85ec0c411b467
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2023-09-21 23:18:37 +0900

    [add]Categoryモデルにバリデーションを追加する

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ApplicationRecord
+  MAX_LENGTH_NAME = 20
+  MAX_LENGTH_HIRAGANA = 20
+
+  validates :name, presence: true, uniqueness: true, length: { maximum: MAX_LENGTH_NAME }
+  validates :hiragana, presence: true, uniqueness: true, length: { maximum: MAX_LENGTH_HIRAGANA }
 end
