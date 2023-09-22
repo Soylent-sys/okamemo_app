243f123100804f9439f47e7cc2e33ae69b06f3bd
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2023-09-22 19:32:01 +0900

    [update]Itemモデル関連のmigrationファイルのmigrateを実施する

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_21_061854) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_22_100506) do
   create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.string "hiragana", default: "", null: false
@@ -18,6 +18,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_21_061854) do
     t.datetime "updated_at", null: false
     t.index ["hiragana"], name: "index_categories_on_hiragana", unique: true
     t.index ["name"], name: "index_categories_on_name", unique: true
+  end
+
+  create_table "items", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.string "name", default: "", null: false
+    t.string "hiragana", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_items_on_category_id"
+    t.index ["hiragana", "user_id", "category_id"], name: "index_items_on_hiragana_and_user_id_and_category_id", unique: true
+    t.index ["name", "user_id", "category_id"], name: "index_items_on_name_and_user_id_and_category_id", unique: true
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -39,4 +52,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_21_061854) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "categories"
+  add_foreign_key "items", "users"
 end
