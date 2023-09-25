b0d57c35ad5863ae8edf51af1a7a397b67abc12d
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2023-09-25 18:11:11 +0900

    [add]Itemモデルにバリデーションを追加する

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  MAX_LENGTH_NAME = 20
+  MAX_LENGTH_HIRAGANA = 20
+  VALID_HIRAGANA_REGEX = /\A[ぁ-んー－]+\z/
+
+  validates :name, presence: true, uniqueness: { scope: [:user_id, :category_id], message: "は同じカテゴリーの中で二つ以上登録できません。" },
+                   length: { maximum: MAX_LENGTH_NAME }
+  validates :hiragana, presence: true, uniqueness: { scope: [:user_id, :category_id], message: "は同じカテゴリーの中で二つ以上登録できません。" },
+                       length: { maximum: MAX_LENGTH_HIRAGANA },
+                       format: { with: VALID_HIRAGANA_REGEX, message: "の項目はひらがなで入力してください。" }
 end
