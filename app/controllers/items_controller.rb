a6ef8f5d2e07a077c445622c7079b865dbec9c3b
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2023-10-03 01:09:33 +0900

    [add]アイテム登録機能を追加する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,30 @@
 class ItemsController < ApplicationController
+  before_action :set_categories, only: [:index, :new, :create]
+
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      flash[:notice] = "アイテム登録が完了しました。"
+      redirect_to items_url
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_categories
     @categories = Category.all
+  end
+
+  def item_params
+    params.require(:item).permit(:user_id, :category_id, :name, :hiragana)
   end
 end
