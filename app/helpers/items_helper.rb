2034a0767ad85f627ac42cdc46f791a72438712a
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2023-10-20 11:40:26 +0900

    [refactor]itemsヘルパーのメソッドのロジックを変更し名称を簡略化する
    対象メソッドのitems変数の式のレシーバーをcurrent_user.itemsからcategory.items（引数を軸にする様）に変更

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,8 +1,8 @@
 module ItemsHelper
   CATEGORY_FIRST_ID = 1
 
-  def current_user_create_items(category)
-    items = current_user.items.where(category_id: category.id)
+  def my_items(category)
+    items = category.items.where(user_id: current_user.id)
     if category.id == CATEGORY_FIRST_ID
       items
     else
