febdb4cfa5c83ca3ec45541e888f24e9681c19a3
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2023-11-09 18:42:47 +0900

    [fix]bootstrap_tooltipがrender後のページでも動くように修正する

--- app/javascript/custom/bootstrap_tooltip.js
+++ app/javascript/custom/bootstrap_tooltip.js
@@ -1,4 +1,7 @@
-document.addEventListener('turbo:load', () => {
+function setTooltip() {
   const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
   const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-});
+}
+
+document.addEventListener('turbo:load', setTooltip);
+document.addEventListener('turbo:render', setTooltip);
