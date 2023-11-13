7ffa4ee65b751515660bf1c0d29a22e32b8112f0
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2023-11-13 18:41:30 +0900

    [add]shopping_records_helper.rbに登録機能のビューで使用するヘルパーメソッドを追加

---
+++ app/helpers/shopping_records_helper.rb
@@ -0,0 +1,49 @@
+module ShoppingRecordsHelper
+  CATEGORY_FIRST_ID = 1
+
+  def categoy_items(category)
+    category_items = category.items.where(user_id: [master_admin_user.id, current_user.id])
+    if category.id == CATEGORY_FIRST_ID
+      category_items
+    else
+      category_items.sorted
+    end
+  end
+
+  def shopping_title(shopping_record_form)
+    if shopping_record_form.title.blank?
+      "#{Date.today.to_s(:date_ja)}のお買い物"
+    else
+      shopping_record_form.title
+    end
+  end
+
+  def last_bought_day(item)
+    last_bought_day = current_user.buys.purchased.where(item_name: item.name).
+      order(updated_at: :desc).pick(:updated_at)
+
+    if last_bought_day.blank?
+      "購入記録なし"
+    elsif Date.current.all_day.cover? last_bought_day
+      "今日購入しています"
+    elsif Date.yesterday.all_day.cover? last_bought_day
+      "昨日購入しています"
+    elsif Date.current - 7.day < last_bought_day.to_date
+      "#{(Date.current - last_bought_day.to_date).to_i}日前に購入"
+    else
+      "#{last_bought_day.to_s(:date_ymd)} 購入"
+    end
+  end
+
+  def wish_items(shopping_record_form)
+    shopping_record_form.hashids.map do |item_hashid|
+      Item.find_by_hashid!(item_hashid)
+    end
+  end
+
+  private
+
+  def master_admin_user
+    User.find_by!(email: "#{ENV['ADMIN_USER_EMAIL']}")
+  end
+end
