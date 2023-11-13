c7e66f7b645e07ba137fc427e0557e7a2784787c
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2023-11-13 18:28:48 +0900

    [add]買い物登録で複数モデルの登録を実施するフォームオブジェクトを作成する

---
+++ app/forms/shopping_record_form.rb
@@ -0,0 +1,26 @@
+class ShoppingRecordForm
+  include ActiveModel::Model
+
+  attr_accessor :shopping_record_id, :user_id, :title, :hashids
+
+  HASHIDS_MINIMUM_SIZE = 1
+  HASHIDS_MAXIMUM_SIZE = 20
+
+  validates :title, presence: true, length: { maximum: ShoppingRecord::MAX_LENGTH_TITLE }
+  validates :hashids, length: {
+    in: HASHIDS_MINIMUM_SIZE..HASHIDS_MAXIMUM_SIZE,
+    too_short: "は#{HASHIDS_MINIMUM_SIZE} つ以上選択してください。",
+    too_long: "のチェック数が#{HASHIDS_MAXIMUM_SIZE} 個を超えています。",
+  }
+
+  def save
+    ActiveRecord::Base.transaction do
+      shopping_record = ShoppingRecord.create!(user_id:, title:)
+      hashids.each do |item_hashid|
+        item = Item.find_by_hashid!(item_hashid)
+        Buy.create!(user_id: shopping_record.user_id, shopping_record_id: shopping_record.id,
+                    item_name: item.name, item_hiragana: item.hiragana)
+      end
+    end
+  end
+end
