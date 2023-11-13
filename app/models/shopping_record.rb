4ef0fb38eff36b7521d6c15e3b34713fe18c53b5
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2023-11-13 18:09:37 +0900

    [add]shopping_recordモデルにhashidを適用しスコープとバリデーションを追加する

--- app/models/shopping_record.rb
+++ app/models/shopping_record.rb
@@ -1,5 +1,14 @@
 class ShoppingRecord < ApplicationRecord
+  include Hashid::Rails
+
   belongs_to :user
   has_one :shopping_location, dependent: :delete
   has_many :buys, dependent: :delete_all
+
+  scope :opened, -> { where(closed: false) }
+  scope :closed, -> { where(closed: true) }
+
+  MAX_LENGTH_TITLE = 40
+
+  validates :title, presence: true, length: { maximum: MAX_LENGTH_TITLE }
 end
