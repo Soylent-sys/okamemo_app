ddc1e798e2388149ecdb14e3c779935bf055045c
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2024-01-31 14:20:32 +0900

    [add]お買い物場所編集機能の処理をshopping_locationsコントローラーに追加する

--- app/controllers/shopping_locations_controller.rb
+++ app/controllers/shopping_locations_controller.rb
@@ -8,7 +8,12 @@ class ShoppingLocationsController < ApplicationController
       redirect_to shopping_result_url
     end
 
-    @shopping_location = ShoppingLocation.new
+    if @shopping_record.shopping_location.blank?
+      @shopping_location = ShoppingLocation.new
+    else
+      # 場所登録済みの場合は編集(edit)にリダイレクトする
+      redirect_to edit_shopping_location_url(@shopping_record.hashid)
+    end
   end
 
   def create
@@ -21,9 +26,37 @@ class ShoppingLocationsController < ApplicationController
     redirect_to shopping_results_url(shopping_record.hashid)
   end
 
+  def edit
+    @shopping_record = current_user.shopping_records.closed.find_by_hashid(params[:id])
+    if @shopping_record.blank?
+      flash[:error] = "指定されたお買い物場所の記録は存在しません。"
+      redirect_to shopping_result_url
+    end
+
+    @shopping_location = @shopping_record.shopping_location
+    if @shopping_location.blank?
+      flash[:error] = "指定されたお買い物場所の記録は存在しません。"
+      redirect_to shopping_result_url
+    end
+
+    gon.lat = @shopping_location.latitude
+    gon.lng = @shopping_location.longitude
+  end
+
+  def update
+    shopping_location = ShoppingLocation.find_by_hashid!(params[:id])
+    shopping_location.update!(update_shopping_location_params)
+    flash[:notice] = "お買い物場所が更新されました。"
+    redirect_to shopping_results_url(shopping_location.shopping_record.hashid)
+  end
+
   private
 
   def shopping_location_params
     params.require(:shopping_location).permit(:shopping_record_id, :latitude, :longitude)
+  end
+
+  def update_shopping_location_params
+    params.require(:shopping_location).permit(:latitude, :longitude)
   end
 end
