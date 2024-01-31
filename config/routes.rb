7c47028b35ede47888c43a92f8fc76d545864b57
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2024-01-31 14:18:25 +0900

    [add]お買い物場所編集機能に必要なルートを追加する

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,8 @@ Rails.application.routes.draw do
   get 'shopping/result', to: 'shopping_records#result'
   get 'shopping/result/:id/show', to: 'shopping_records#show', as: 'shopping_results'
   get 'shopping/result/:id/location/new', to: 'shopping_locations#new', as: 'new_shopping_location'
+  get 'shopping/result/:id/location/edit', to: 'shopping_locations#edit', as: 'edit_shopping_location'
   resources :shopping_records, only: [:create, :update, :destroy]
-  resources :shopping_locations, only: [:create]
+  resources :shopping_locations, only: [:create, :update]
   resources :items, only: [:index, :new, :create, :edit, :update, :destroy]
 end
