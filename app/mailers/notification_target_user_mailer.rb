4314dff385658153f2f2e5637adf15ce9f730e90
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2024-03-08 16:00:37 +0900

    [add]通知対象ユーザー登録機能に使用するメーラーを作成する

---
+++ app/mailers/notification_target_user_mailer.rb
@@ -0,0 +1,6 @@
+class NotificationTargetUserMailer < ApplicationMailer
+  def send_email_confirmation
+    @notification_target_user = params[:nt_user]
+    mail(to: @notification_target_user.email, subject: 'メールアドレス認証のお願い')
+  end
+end
---
+++ spec/mailers/previews/notification_target_user_mailer_preview.rb
@@ -0,0 +1,3 @@
+# Preview all emails at http://localhost:3000/rails/mailers/notification_target_user_mailer
+class NotificationTargetUserMailerPreview < ActionMailer::Preview
+end
