d8e814bb19f911fd71670d96339ccf5a99006b89
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2024-03-12 19:07:38 +0900

    [add]お買い物通知機能関連のメールプレビューを追加する

--- spec/mailers/previews/notification_target_user_mailer_preview.rb
+++ spec/mailers/previews/notification_target_user_mailer_preview.rb
@@ -1,3 +1,7 @@
-# Preview all emails at http://localhost:3000/rails/mailers/notification_target_user_mailer
+# Preview all emails at http://localhost:80/rails/mailers/notification_target_user_mailer
 class NotificationTargetUserMailerPreview < ActionMailer::Preview
+  def send_email_confirmation
+    NotificationTargetUserMailer.with(nt_user: User.master_admin_user.notification_target_users.unconfirmed.first).
+      send_email_confirmation
+  end
 end
--- spec/mailers/previews/shopping_record_mailer_preview.rb
+++ spec/mailers/previews/shopping_record_mailer_preview.rb
@@ -1,3 +1,8 @@
-# Preview all emails at http://localhost:3000/rails/mailers/shopping_record_mailer
+# Preview all emails at http://localhost:80/rails/mailers/shopping_record_mailer
 class ShoppingRecordMailerPreview < ActionMailer::Preview
+  def send_shopping_result
+    user = User.master_admin_user
+    ShoppingRecordMailer.with(shopping_record: user.shopping_records.first,
+                              nt_user: user.notification_target_users.confirmed.first).send_shopping_result
+  end
 end
