e72954d4dfb81c1aac376aba3a2882f94b99945d
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2024-04-05 17:03:37 +0900

    [update]日本語の日付フォーマットで月が一桁の時に先頭の0を表示しない様に改善する

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,7 +1,8 @@
 # 日時表示用フォーマット
-Date::DATE_FORMATS[:date_ja] = '%Y年%m月%d日'
-Time::DATE_FORMATS[:month_ja] = '%Y年%m月'
+Date::DATE_FORMATS[:date_ja] = '%Y年 %-m月%-d日'
+Time::DATE_FORMATS[:month_ja] = '%Y年 %-m月'
 Time::DATE_FORMATS[:date_ymd] = '%Y/%m/%d'
+Time::DATE_FORMATS[:date_ymd_ja] = '%Y年 %-m月%-d日'
 
 # 月別グルーピング用フォーマット
 Time::DATE_FORMATS[:date_ym] = '%Y-%m'
