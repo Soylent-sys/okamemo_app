1f26707d6b0e64a549e95060f545973859f25b65
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2024-04-08 20:43:03 +0900

    [refactor]routes.rbの一部ルートの並びを見やすくなるように改善する

--- config/routes.rb
+++ config/routes.rb
@@ -14,18 +14,18 @@ Rails.application.routes.draw do
     edit: 'edit/profile',
   }
   root 'home#index'
-  get 'shopping/new', to: 'shopping_records#new'
-  post 'shopping/new/back', to: 'shopping_records#back_new'
-  post 'shopping/new/confirm', to: 'shopping_records#confirm'
-  get 'shopping/index', to: 'shopping_records#index'
-  get 'shopping/:id/progress', to: 'shopping_records#edit', as: 'shopping_progress'
-  post 'shopping/progress/back', to: 'shopping_records#back_edit'
-  post 'shopping/progress/confirm', to: 'shopping_records#edit_confirm'
-  get 'shopping/result_group', to: 'shopping_records#result_group'
-  get 'shopping/result/:date', to: 'shopping_records#result', as: 'shopping_result'
-  get 'shopping/result/:id/show', to: 'shopping_records#show', as: 'shopping_results'
-  get 'shopping/result/:id/location/new', to: 'shopping_locations#new', as: 'new_shopping_location'
-  get 'shopping/result/:id/location/edit', to: 'shopping_locations#edit', as: 'edit_shopping_location'
+  get  'shopping/new',                      to: 'shopping_records#new'
+  post 'shopping/new/back',                 to: 'shopping_records#back_new'
+  post 'shopping/new/confirm',              to: 'shopping_records#confirm'
+  get  'shopping/index',                    to: 'shopping_records#index'
+  get  'shopping/:id/progress',             to: 'shopping_records#edit', as: 'shopping_progress'
+  post 'shopping/progress/back',            to: 'shopping_records#back_edit'
+  post 'shopping/progress/confirm',         to: 'shopping_records#edit_confirm'
+  get  'shopping/result_group',             to: 'shopping_records#result_group'
+  get  'shopping/result/:date',             to: 'shopping_records#result',      as: 'shopping_result'
+  get  'shopping/result/:id/show',          to: 'shopping_records#show',        as: 'shopping_results'
+  get  'shopping/result/:id/location/new',  to: 'shopping_locations#new',       as: 'new_shopping_location'
+  get  'shopping/result/:id/location/edit', to: 'shopping_locations#edit',      as: 'edit_shopping_location'
   resources :shopping_records, only: [:create, :update, :destroy]
   resources :shopping_locations, only: [:create, :update, :destroy]
   resources :items, only: [:index, :new, :create, :edit, :update, :destroy]
