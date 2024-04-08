b2eb42663526974237f9a5a27e2faa13fe0a4db7
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2024-04-08 15:06:31 +0900

    [add]ヘッダー部にユーザーの状態を表示する機能を追加する

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,34 @@ module ApplicationHelper
   def html_safe_newline(str)
     h(str).gsub(/\n|\r|\r\n/, "<br>").html_safe
   end
+
+  def position_flash
+    if user_signed_in?
+      "position-flash"
+    else
+      "position-flash-no-login"
+    end
+  end
+
+  def now_info
+    path = request.path
+    case
+    when path.include?("users/edit")
+      "ユーザー編集中！"
+    when path.include?("shopping/new")
+      "お買い物登録中！"
+    when path.include?("progress")
+      "お買い物中！"
+    when path.include?("location")
+      "マップ記録中！"
+    when path.include?("notification_target_users/new")
+      "通知ユーザー登録中！"
+    when path.include?("items/new")
+      "アイテム登録中！"
+    when /items\/\w+\/edit/.match?(path)
+      "アイテム編集中！"
+    else
+      "ログイン中！"
+    end
+  end
 end
