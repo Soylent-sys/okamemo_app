5015e9113eee47973850addf600b5d0a21dd8655
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2024-05-13 17:59:18 +0900

    [add]利用規約・プライバシーポリシーのページを追加する

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
     edit: 'edit/profile',
   }
   root 'home#index'
+  get  '/terms',                                to: 'static_pages#terms'
+  get  '/policy',                               to: 'static_pages#policy'
   get  'shopping/new',                          to: 'shopping_records#new'
   post 'shopping/new/back',                     to: 'shopping_records#back_new'
   post 'shopping/new/confirm',                  to: 'shopping_records#confirm'
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe "StaticPages", type: :request do
+  describe "GET /policy" do
+    it "returns http success" do
+    end
+  end
+end
