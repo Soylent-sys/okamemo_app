65133158347311eb2ac11f02433a464c07c7397b
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2024-05-27 14:03:54 +0900

    [update]お問い合わせフォームからのメール内容に送信者のユーザー登録状態とidを追加する

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,7 @@
 class ContactMailer < ApplicationMailer
   def send_contact_mail
     @contact = params[:contact]
+    @user = User.find_by(email: @contact.email)
     mail(from: "おかメchan お問い合わせフォーム <no-reply@okamemo.com>", to: ENV['CONTACT_EMAIL'], subject: "【お問い合わせ】#{@contact.subject}")
   end
 end
