ee2499f2ff1bfa4ebc74ca90dabbf727ba220c40
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2024-06-05 16:17:51 +0900

    [add]管理画面に表示する日時用のフォーマットを追加する

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -3,6 +3,7 @@ Date::DATE_FORMATS[:date_ja] = '%Y年 %-m月%-d日'
 Time::DATE_FORMATS[:month_ja] = '%Y年 %-m月'
 Time::DATE_FORMATS[:date_ymd] = '%Y/%m/%d'
 Time::DATE_FORMATS[:date_ymd_ja] = '%Y年 %-m月%-d日'
+Time::DATE_FORMATS[:date_time] = '%Y/%m/%d %H:%M:%S'
 
 # 月別グルーピング用フォーマット
 Time::DATE_FORMATS[:date_ym] = '%Y-%m'
