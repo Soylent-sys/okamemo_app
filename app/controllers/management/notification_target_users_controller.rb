8786c83ffeaec03f550ab2894e65865c08fde490
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2024-06-10 22:00:28 +0900

    [add]管理者機能の通知ユーザー管理用コントローラーを作成する

---
+++ app/controllers/management/notification_target_users_controller.rb
@@ -0,0 +1,33 @@
+class Management::NotificationTargetUsersController < ApplicationController
+  include Pagy::Backend
+
+  NT_USERS_PAGENATION_SIZE = 50
+  SINGLE_PAGE = 1
+
+  def index
+    @pagy, @notification_target_users = pagy(NotificationTargetUser.all, items: NT_USERS_PAGENATION_SIZE, size: [1, 2, 2, 1])
+  end
+
+  def new
+    @notification_target_user = NotificationTargetUser.new
+  end
+
+  def create
+    @notification_target_user = NotificationTargetUser.new(notification_target_user_params)
+    if @notification_target_user.save
+      flash[:notice] = "通知ユーザーの登録が完了しました。"
+      redirect_to management_notification_target_users_url
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def notification_target_user_params
+    params.require(:notification_target_user).permit(:user_id, :name, :email, :confirmation_status)
+  end
+end
