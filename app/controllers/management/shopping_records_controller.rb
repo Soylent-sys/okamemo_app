3cf0efbf44e56458dfc7652c99544fa531088a26
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2024-06-13 18:22:32 +0900

    [add]お買い物管理機能のお買い物削除機能を追加する

--- app/controllers/management/shopping_records_controller.rb
+++ app/controllers/management/shopping_records_controller.rb
@@ -11,5 +11,9 @@ class Management::ShoppingRecordsController < ApplicationController
   end
 
   def destroy
+    shopping_record = ShoppingRecord.find(params[:id])
+    shopping_record.destroy!
+    flash[:notice] = "お買い物の削除が完了しました。"
+    redirect_to management_shopping_records_url
   end
 end
