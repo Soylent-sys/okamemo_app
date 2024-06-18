2858b97beb494f1a766278c012c60ae3e132dbd4
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2024-06-18 12:43:30 +0900

    [add]ransackの設定を実施するためconfig用ファイルを作成する

---
+++ config/initializers/ransack.rb
@@ -0,0 +1,7 @@
+Ransack.configure do |c|
+  c.custom_arrows = {
+    up_arrow: '<i class="fas fa-sort-down"></i>',
+    down_arrow: '<i class="fas fa-sort-up"></i>',
+    default_arrow: '<i class="fas fa-sort"></i>'
+  }
+end
