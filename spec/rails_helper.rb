0470ea9c36010c83b098c9e1c021ba899fa509e2
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2024-08-15 18:36:22 +0900

    [add]FactoryBotメソッドでモジュール名 (FactoryBot)の記述を省略可能にする設定を追加する

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,4 +60,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # FactoryBotメソッド使用時にモジュール名 (FactoryBot)を省略できるようする
+  config.include FactoryBot::Syntax::Methods
 end
