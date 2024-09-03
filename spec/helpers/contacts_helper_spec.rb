91e532f58f6f1990d3b35f76a94618e450af39d2
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2024-09-03 12:22:24 +0900

    [test]ContactsHelperのヘルパースペックを追加する

---
+++ spec/helpers/contacts_helper_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+RSpec.describe ContactsHelper, type: :helper do
+  describe "#form_info_text_name" do
+    context "サインインしている場合" do
+      let(:user) { create(:user) }
+
+      before do
+        sign_in user
+      end
+
+      it "サインイン時用の文字列を返すこと" do
+        expect(helper.form_info_text_name).to eq "ご登録のニックネームで送信されます"
+      end
+    end
+
+    context "サインインしていない場合" do
+      it "非サインイン時用の文字列を返すこと" do
+        expect(helper.form_info_text_name).to eq "お名前は最大#{UserSharedConstants::MAX_LENGTH_NAME}文字まで入力できます"
+      end
+    end
+  end
+
+  describe "#form_info_text_email" do
+    context "サインインしている場合" do
+      let(:user) { create(:user) }
+
+      before do
+        sign_in user
+      end
+
+      it "サインイン時用の文字列を返すこと" do
+        expect(helper.form_info_text_email).to eq "ご登録のメールアドレスにご連絡いたします"
+      end
+    end
+
+    context "サインインしていない場合" do
+      it "非サインイン時用の文字列を返すこと" do
+        expect(helper.form_info_text_email).to eq "このメールアドレスにご連絡いたします"
+      end
+    end
+  end
+
+  describe "#done_text" do
+    context "サインインしている場合" do
+      let(:user) { create(:user) }
+
+      before do
+        sign_in user
+      end
+
+      it "サインイン時用の文字列を返すこと" do
+        expect(helper.done_text).to eq "お問い合わせの内容を確認・受付の後、ご登録のEメールアドレスへご連絡いたします。しばらくお待ちください。"
+      end
+    end
+
+    context "サインインしていない場合" do
+      it "非サインイン時用の文字列を返すこと" do
+        expect(helper.done_text).to eq "お問い合わせの内容を確認・受付の後、ご入力いただいたEメールアドレスへご連絡いたします。しばらくお待ちください。"
+      end
+    end
+  end
+end
