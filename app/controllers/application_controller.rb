e340b9f816a2fcd3c2d45e4a683102679123bb05
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2024-10-16 16:19:41 +0900

    [add]実行環境でrenderのステータスコードを分岐するメソッドを追加する
    see_otherによるrenderがRSpec上で動かない為テスト環境ではokでrenderして開発・本番環境と同じ動きにするメソッドを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  # see_otherなどのリダイレクトのステータスコードを付けてrenderすると
+  # RSpec上ではリダイレクトできないためテスト環境のみステータスコードを
+  # okにしてRSpec上でも開発・本番環境と同じ動作をするようにする
+  def render_with_status_see_other(template)
+    if Rails.env.test?
+      render template, status: :ok
+    else
+      render template, status: :see_other
+    end
+  end
 end
