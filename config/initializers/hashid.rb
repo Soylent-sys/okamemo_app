6d4cef828bddf7eced147491420f3d40aae95c56
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2025-02-28 19:40:39 +0900

    [update]環境変数の呼び出しに冗長な文字列補間を再度見つけたため修正する

--- config/initializers/hashid.rb
+++ config/initializers/hashid.rb
@@ -1,5 +1,5 @@
 Hashid::Rails.configure do |config|
-  config.salt = "#{ENV['HASHID_SALT_CHAR']}"
+  config.salt = ENV["HASHID_SALT_CHAR"]
   config.min_hash_length = 10
   config.alphabet = "abcdefghijklmnopqrstuvwxyz" \
   "1234567890"
