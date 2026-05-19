934ae2f5fc8de094814e6893a905958ad50d35f2
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2026-05-19 15:28:41 +0900

    [refactor]helper内の不要なcountクエリを削減する

--- spec/helpers/notification_target_users_helper_spec.rb
+++ spec/helpers/notification_target_users_helper_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe NotificationTargetUsersHelper, type: :helper do
 
     context "通知対象ユーザー数が上限に達している場合" do
       it "disabled の文字列を返すこと" do
-        notification_target_users = double("notification_target_users", count: maximum_count)
+        notification_target_users = double("notification_target_users", size: maximum_count)
         result = helper.disabled_if_limited(notification_target_users)
         expect(result).to eq("disabled")
       end
@@ -14,7 +14,7 @@ RSpec.describe NotificationTargetUsersHelper, type: :helper do
 
     context "通知対象ユーザー数が上限を超えている場合" do
       it "disabled の文字列を返すこと" do
-        notification_target_users = double("notification_target_users", count: maximum_count + 1)
+        notification_target_users = double("notification_target_users", size: maximum_count + 1)
         result = helper.disabled_if_limited(notification_target_users)
         expect(result).to eq("disabled")
       end
@@ -22,7 +22,7 @@ RSpec.describe NotificationTargetUsersHelper, type: :helper do
 
     context "通知対象ユーザー数が上限に満たない場合" do
       it "nil を返すこと" do
-        notification_target_users = double("notification_target_users", count: maximum_count - 1)
+        notification_target_users = double("notification_target_users", size: maximum_count - 1)
         result = helper.disabled_if_limited(notification_target_users)
         expect(result).to be_nil
       end
